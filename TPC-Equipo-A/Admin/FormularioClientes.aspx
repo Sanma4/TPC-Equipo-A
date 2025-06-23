79ef4b560b50d8881dbadbdf8b9c087f7e4ff034
sanma4 <sanmartinobenjamin@gmail.com>
2025-06-23 15:31:24 +0200

    Agregado Front de Formulario Clientes

---
+++ TPC-Equipo-A/Admin/FormularioClientes.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FormularioClientes.aspx.cs" Inherits="TPC_Equipo_A.Admin.FormularioClientes" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <div class="col-md-4 offset-md-2">
+            <div class="mb-3">
+                <label for="txtNombre" class="form-label">Nombre</label>
+                <asp:TextBox ID="txtNombre" CssClass="form-control" runat="server"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label for="txtApellido" class="form-label">Apellido</label>
+                <asp:TextBox ID="txtApellido" CssClass="form-control" runat="server"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label for="txtEmail" class="form-label">Email</label>
+                <asp:TextBox ID="txtEmail" CssClass="form-control" runat="server"></asp:TextBox>
+            </div>
+        </div>
+        <div class="col-md-4 ">
+            <div class="mb-3">
+                <label for="txtTlf" class="form-label">Teléfono</label>
+                <asp:TextBox ID="txtTlf" CssClass="form-control" runat="server"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label for="txtSexo" class="form-label">Sexo</label>
+                <asp:TextBox ID="txtSexo" CssClass="form-control" placeholder="Ingresar M-F" runat="server"></asp:TextBox>
+            </div>
+            <div class="mb-3">
+                <label for="txtActivo" class="form-label">Activo</label>
+                <asp:DropDownList ID="txtActivo" CssClass="form-control" runat="server"></asp:DropDownList>
+            </div>
+        </div>
+    </div>
+    <asp:Button ID="btnAceptar" runat="server" Text="Aceptar" CssClass="btn btn-dark" />
+    <a href="ListaClientes.aspx" class="btn btn-primary">Volver</a>
+</asp:Content>
---
+++ TPC-Equipo-A/Admin/FormularioClientes.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_Equipo_A.Admin
+{
+
+
+    public partial class FormularioClientes
+    {
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellido;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control txtTlf.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTlf;
+
+        /// <summary>
+        /// Control txtSexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSexo;
+
+        /// <summary>
+        /// Control txtActivo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList txtActivo;
+
+        /// <summary>
+        /// Control btnAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAceptar;
+    }
+}
