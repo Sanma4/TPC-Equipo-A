5d5eccd424707344a7e4604d3dae57e080fa27c4
FAZI-Franco-David <fazifrancodavid@outlook.com>
2025-06-29 18:25:53 -0300

    se carga validaciones a btnAceptar

--- TPC-Equipo-A/Admin/FormularioPeluqueros.aspx.cs
+++ TPC-Equipo-A/Admin/FormularioPeluqueros.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace TPC_Equipo_A.Admin
 {
@@ -13,5 +15,43 @@ namespace TPC_Equipo_A.Admin
         {
 
         }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            PeluqueroNegocio negocio = new PeluqueroNegocio();
+            Peluquero nuevo = new Peluquero();
+
+            try
+            {
+                nuevo.Nombre = txtNombre.Text;
+                nuevo.Apellido = txtApellido.Text;
+                nuevo.Telefono = txtTlf.Text;
+                nuevo.Email = txtEmail.Text;
+                nuevo.Sexo = txtSexo.Text;
+                if (ddlActivo.SelectedValue == "Si")
+                    nuevo.Activo = true;
+                else
+                    nuevo.Activo = false;
+
+
+                if (Request.QueryString["id"] != null)
+                {
+                    nuevo.Id = int.Parse(txtId.Text);
+                    negocio.Editar(nuevo);
+                }
+                else
+                {
+                    negocio.Agregar(nuevo);
+                }
+
+                Response.Redirect("ListaPeluqueros.aspx", false);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
     }
 }
